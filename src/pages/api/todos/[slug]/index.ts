5364c8dc5e26e0bcdff6b5f1b706ac9690a94652
Jacob Kyalo <jacobkyalo43@gmail.com>
2023-06-22 13:04:19 +0300

    initial commit

---
+++ src/pages/api/todos/[slug]/index.ts
@@ -0,0 +1,14 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { todos } from "../../../../../todos";
+
+type Todo = {
+  // id: number;
+  title: string;
+};
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Todo>
+) {
+  res.status(200).json({ title: "todo id" });
+}
---
+++ todos.ts
@@ -0,0 +1,23 @@
+export const todos = [
+  {
+    id: 1,
+    title: "Do something nice for someone I care about",
+  },
+  {
+    id: 2,
+    title: "Memorize the fifty states and their capitals",
+  },
+  {
+    id: 3,
+    title: "Watch a classic movie",
+  },
+  {
+    id: 4,
+    title:
+      "Contribute code or a monetary donation to an open-source software project",
+  },
+  {
+    id: 5,
+    title: "Solve a Rubik's cube",
+  },
+];
