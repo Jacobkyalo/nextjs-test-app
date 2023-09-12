ddefe00fd63aff917ae2419abe77f4c9cc941587
Jacob Kyalo <jacobkyalo43@gmail.com>
2023-09-12 17:14:20 +0300

    another commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,40 +1,20 @@
-import { GetStaticProps, InferGetStaticPropsType } from "next";
 import { Inter } from "next/font/google";
 import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
-type Todo = {
-  id: number;
-  title: string;
-};
-
-export default function Home({ todos }: InferGetStaticPropsType<typeof getStaticProps>) {
+export default function Home() {
   return (
     <>
       <Head>
-        <title>Nextjs Testing App</title>
+        <title>Nextjs Hello World Testing App</title>
       </Head>
       <main className={`${inter.className} text-center`}>
-        <h1 className="mt-12 text-5xl text-center">Hello Nextjs</h1>
-        <h2 className="my-6 text-2xl">List of todos</h2>
-        <ul>
-          {todos.map((todo: Todo) => (
-            <li key={todo.id}>{todo.title}</li>
-          ))}
-        </ul>
+        <h1 className="mt-12 text-5xl text-center">
+          Hello Nextjs + Typescript
+        </h1>
+        <p className="mt-10 text-xl opacity-80">Nice to meet you</p>
       </main>
     </>
   );
 }
-
-export const getStaticProps: GetStaticProps<{ todos: Todo[] }> = async () => {
-  const res = await fetch("http://localhost:3000/api/todos");
-  const todos = await res.json();
-
-  return {
-    props: {
-      todos,
-    },
-  };
-};
